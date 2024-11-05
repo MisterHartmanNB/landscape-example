8081de77fdce790834e8cce0b30ab5d8bede72f0
John Hartman <john.hartman@newbremenschools.org>
2024-11-05 18:12:12 +0000

    Used copilot to add clouds in the day, a house in the day, and a tree at both day and night.

--- script.js
+++ script.js
@@ -17,6 +17,32 @@ var grass = new Rectangle(getWidth(), getHeight() / 4);
 grass.setPosition(0, getHeight() * 3 / 4);
 grass.setColor('green');
 add(grass);
+
+// Draw the house only if it's daytime
+if (timeOfDay === "daytime") {
+    var house = new Rectangle(100, 100);
+    house.setPosition(getWidth() - 150, getHeight() * 3 / 4 - 100);
+    house.setColor('red');
+    add(house);
+
+    var roof = new Polygon();
+    roof.addPoint(getWidth() - 150, getHeight() * 3 / 4 - 100);
+    roof.addPoint(getWidth() - 50, getHeight() * 3 / 4 - 100);
+    roof.addPoint(getWidth() - 100, getHeight() * 3 / 4 - 160);
+    roof.setColor('brown');
+    add(roof);
+}
+
+// Draw the tree
+var trunk = new Rectangle(20, 100);
+trunk.setPosition(getWidth() / 4, getHeight() * 3 / 4 - 100);
+trunk.setColor('brown');
+add(trunk);
+
+var leaves = new Circle(50);
+leaves.setPosition(getWidth() / 4 + 10, getHeight() * 3 / 4 - 150);
+leaves.setColor('green');
+add(leaves);
 }
 
 // Function to draw the daytime sky
@@ -25,6 +51,14 @@ function drawDaytimeSky() {
     sky.setPosition(0, 0);
     sky.setColor('skyBlue');
     add(sky);
+
+    //Draw random clouds using a loop
+    for (var i = 0; i < 5; i++) {
+        var cloud = new Circle(30);
+        cloud.setPosition(Randomizer.nextInt(0, getWidth()), Randomizer.nextInt(0, getHeight() / 2));
+        cloud.setColor('white');
+        add(cloud);
+    }
 
     // Draw the sun
     var sun = new Circle(50);
@@ -40,12 +74,6 @@ function drawNighttimeSky() {
     sky.setColor('black');
     add(sky);
 
-    // Draw the moon
-    var moon = new Circle(50);
-    moon.setPosition(getWidth() - 100, 100);
-    moon.setColor('gray');
-    add(moon);
-
     // Draw stars using a loop
     for (var i = 0; i < 50; i++) {
         var star = new Circle(2);
@@ -53,6 +81,12 @@ function drawNighttimeSky() {
         star.setColor('white');
         add(star);
     }
+
+        // Draw the moon
+        var moon = new Circle(50);
+        moon.setPosition(getWidth() - 100, 100);
+        moon.setColor('gray');
+        add(moon);
 }
 
 main();
